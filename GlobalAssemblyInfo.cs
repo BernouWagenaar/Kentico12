e818c7ff2443245854dec4e0556ca565585e1d84
BernouWagenaar <Bernou.Wagenaar@AvivaSolutions.nl>
2019-05-18 18:16:09 +0200

    Added sample site templates and applied hotfix

--- GlobalAssemblyInfo.cs
+++ GlobalAssemblyInfo.cs
@@ -5,5 +5,5 @@
 [assembly: AssemblyCopyright("")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyVersion("12.0.0.0")]
-[assembly: AssemblyFileVersion("12.0.6900.17470")]
-[assembly: AssemblyInformationalVersion("12.0.0")]
+[assembly: AssemblyFileVersion("12.0.7034.28880")]
+[assembly: AssemblyInformationalVersion("12.0.17")]
